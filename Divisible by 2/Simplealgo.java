c50bcc927f565c5c09006e8e7008c32f038fc40d
arai639 <44468381+arai639@users.noreply.github.com>
2018-10-25 19:34:06 +0530

    Create Simplealgo.java

---
+++ Divisible by 2/Simplealgo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class div
+{
+public static void main(String arg[])
+{
+Scanner ob=new Scanner(System.in);
+System.out.println("Enter the number");
+int n = ob.nextInt();
+if(n%2==0)
+System.out.println("Divisible by 2");
+else
+System.out.println("Not Divisible by 2");
+}
+}
